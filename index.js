b099d24b065f248fde6b9e7728baa1833bc70b00
hanks-tan <hiker7@hotmail.com>
2021-05-18 10:08:08 +0800

    第一次提交

---
+++ index.js
@@ -0,0 +1,40 @@
+import { Collection, Feature } from 'ol'
+import GeoJSON from 'ol/format/GeoJSON'
+import Geometry from 'ol/geom/geometry'
+
+export function ol2turf (olData) {
+  let format = new GeoJSON()
+  if (olData instanceof Feature) {
+    return format.writeFeatureObject(olData)
+  } else if (olData instanceof Geometry) {
+    return format.writeGeometryObject(olData)
+  } else if (olData instanceof Collection) {
+    let dataList = olData.getArray()
+    if (dataList[0] instanceof Feature) {
+      return format.writeFeaturesObject(dataList)
+    }
+  }
+  return null
+}
+
+export function turf2ol (turfData) {
+  let format = new GeoJSON()
+  if (turfData.type === 'Feature') {
+    return format.readFeature(turfData)
+  } else if (['Point', 'MultiPoint', 'LineString', 'MultiLineString', 'Polygon', 'MultiPolygon'].indexOf(turfData.type) > -1) {
+    return format.readGeometry(turfData)
+  } else if (turfData.type === 'FeatureCollection') {
+    return format.readFeatures(turfData)
+  } else if (turfData.type === 'GeometryCollection') {
+    return turfData.geometries.map((geom) => {
+      return format.readGeometry(geom)
+    })
+  } else {
+    return null
+  }
+}
+
+export default {
+  ol2turf,
+  turf2ol
+}
